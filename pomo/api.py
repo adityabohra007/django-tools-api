fd9c1f36bc42b15ebf3c1c57112743acbf846f84
Aditya <adityabohra619@gmail.com>
2023-12-09 20:37:03 +0530

    Google Authentication added

--- pomo/api.py
+++ pomo/api.py
@@ -12,6 +12,16 @@ from .utils import check_if_any_timer_already_active
 from django.db.models import Q
 # right now working on guest user only 
 
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+
+
+class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://127.0.0.1:8081/accounts/google/login/callback/'
+    client_class = OAuth2Client
+
 class InitialViewApi(APIView):
     ''' Timer if any running
         All task list
--- tools/urls.py
+++ tools/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from pomo.api import GoogleLogin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('excel_reduction/',include('excel_reduction.urls')),
     path("pomo/",include('pomo.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("dj-rest-auth/google", GoogleLogin.as_view(), name="")
 ]
